b2196025ad8b599e0a6be184803efe20fa2a4247
ihorborys <ihorborys@gmail.com>
2024-07-07 15:48:27 +0300

    Added task 1

---
+++ task_1.py
@@ -0,0 +1,22 @@
+from datetime import date, datetime
+"""Transforms date string to date object
+Throws:
+    ValueError, TypeError when date format is not: %Y-%m-%d
+"""
+def transform_string_to_date(date: str) -> date:
+    try:
+        return datetime.strptime(date, "%Y-%m-%d").date()
+    except (ValueError, TypeError):
+        print("Please, enter data in format: 'YYYY-MM-DD'")
+
+"""Calculates distance between dates"""
+def get_days_from_today(date_one: date, date_two: date) -> int:
+    try:
+        return (date_two - date_one).days
+    except (ValueError, TypeError):
+        print("Please, enter data in format: 'YYYY-MM-DD'")
+
+date_one = transform_string_to_date("1988-10-30")
+date_two = datetime.now().date()
+
+print(get_days_from_today(date_one, date_two))
